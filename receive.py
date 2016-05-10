648cf480eeda262e16365cb0c430b6bed3acd250
David Capalbo <akadmarccc@gmail.com>
2016-05-09 21:16:19 -0400

    cleaned up

--- receive.py
+++ receive.py
@@ -1,20 +1,20 @@
+# pika package required
 import pika
 
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost'))
+# wait for connection before continuing 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+# get the channel class
 channel = connection.channel()
-
-
-channel.queue_declare(queue='hello')
-
+# declare queue in case doesnt exist
+channel.queue_declare(queue='test_queue')
+# define the callback function
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-
+# bind the callback function
 channel.basic_consume(callback,
-                  queue='hello',
+                  queue='test_queue',
                   no_ack=True)
 
-
 print(' [*] Waiting for messages. To exit press CTRL+C')
+# begin waiting for messages and consuming them
 channel.start_consuming()
--- send.py
+++ send.py
@@ -1,13 +1,19 @@
+# pika package required
 import pika
 
+# wait for connection before continuing 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# get the channel class
 channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
+# declare the rabbitMQ queue, create if neccesary. 
+channel.queue_declare(queue='test_queue')
+# publish message to an queue
 channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='im the worlds greatest')
-print(" [x] Sent 'Hello World!'")
+					  # routing key is queue name
+                      routing_key='test_queue',
+                      # body is message
+                      body='wait a minute mr postman')
 
+# notify and close connection
+print("Message Sent")
 connection.close()
